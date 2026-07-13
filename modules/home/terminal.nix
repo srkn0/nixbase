ac6dc7e7483f10fd22b86fe31b7b53ac4f23d861
Serkan k <serkan-koese@gmx.de>
2026-07-14 01:20:11 +0200

    fix kitty: inline gruvbox dark colors, drop themeFile

--- modules/home/terminal.nix
+++ modules/home/terminal.nix
@@ -13,7 +13,29 @@
       update_check_interval   = 0;
       confirm_os_window_close = 0;
     };
-    themeFile = "Gruvbox Dark";
+    extraConfig = ''
+      # Gruvbox Dark
+      background            #282828
+      foreground            #ebdbb2
+      cursor                #ebdbb2
+      selection_background  #d65d0e
+      color0                #282828
+      color1                #cc241d
+      color2                #98971a
+      color3                #d79921
+      color4                #458588
+      color5                #b16286
+      color6                #689d6a
+      color7                #a89984
+      color8                #928374
+      color9                #fb4934
+      color10               #b8bb26
+      color11               #fabd2f
+      color12               #83a598
+      color13               #d3869b
+      color14               #8ec07c
+      color15               #ebdbb2
+    '';
   };
 
   programs.tmux = {
