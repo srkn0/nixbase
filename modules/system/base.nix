16b338427850d32caa56ada3a6fb0046ffaada1a
Serkan k <serkan-koese@gmx.de>
2026-07-14 00:03:41 +0200

    add system modules: base, gnome, nvidia, docker

---
+++ modules/system/base.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "de_DE.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS        = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT    = "de_DE.UTF-8";
+    LC_MONETARY       = "de_DE.UTF-8";
+    LC_NAME           = "de_DE.UTF-8";
+    LC_NUMERIC        = "de_DE.UTF-8";
+    LC_PAPER          = "de_DE.UTF-8";
+    LC_TELEPHONE      = "de_DE.UTF-8";
+    LC_TIME           = "de_DE.UTF-8";
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+}
---
+++ modules/system/gnome.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  services.xserver.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  services.xserver.xkb = {
+    layout  = "de";
+    variant = "";
+  };
+  console.keyMap = "de";
+
+  services.printing.enable = true;
+  services.flatpak.enable = true;
+  programs.firefox.enable = true;
+
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable            = true;
+    alsa.enable       = true;
+    alsa.support32Bit = true;
+    pulse.enable      = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.paperwm
+    gnome-tweaks
+  ];
+}
