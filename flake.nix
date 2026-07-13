7be2afbd16af293b5085aeadac9043f946b0fd73
Serkan k <serkan-koese@gmx.de>
2026-07-14 00:37:30 +0200

    upgrade nixpkgs + home-manager to 26.05

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS configuration — modular, two hosts, two users each";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
