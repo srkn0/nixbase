6a2358c9b5a7e3b87997f3865b64f3fd59c7756a
srkn0 <srkn0@github>
2026-07-14 00:03:41 +0200

    add system modules: base, gnome, nvidia, docker

---
+++ modules/system/nvidia.nix
@@ -0,0 +1,16 @@
+{ config, ... }:
+
+{
+  hardware.graphics.enable = true;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable         = true;
+    powerManagement.enable     = false;
+    powerManagement.finegrained = false;
+    open           = true;
+    nvidiaSettings = true;
+    package        = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
