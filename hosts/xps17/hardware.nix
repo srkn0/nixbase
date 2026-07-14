54c5403be0c0c7c184dac7b63165a8442b97e65a
srkn0 <srkn0@github>
2026-07-14 17:17:37 +0000

    update xps17 hardware.nix

--- hosts/xps17/hardware.nix
+++ hosts/xps17/hardware.nix
@@ -2,18 +2,19 @@
 # `nixos-generate-config --no-filesystems --root /mnt` and paste only the
 # hardware-detected bits below (kernel modules etc.), not any fileSystems.*
 # or swapDevices.
-{ lib, modulesPath, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ ];
-  boot.initrd.kernelModules          = [ ];
-  boot.kernelModules                 = [ ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules          = [ "dm_snapshot" ];
+  boot.kernelModules                 = [ "kvm-intel" ];
   boot.extraModulePackages           = [ ];
 
   networking.useDHCP    = lib.mkDefault true;
   nixpkgs.hostPlatform  = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
